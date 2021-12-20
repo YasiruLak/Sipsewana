0cc6295eac6fbe19045f3b1983f64c2d6d752151
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2021-12-20 22:43:59 +0530

    Property File Configuration Done

---
+++ src/lk/ijse/sipsewana/bo/SuperBO.java
@@ -0,0 +1,12 @@
+package lk.ijse.sipsewana.bo;
+
+/**
+ * @author : Yasiru Dahanayaka
+ * @name : Sipsewana
+ * @date : 12/20/2021
+ * @month : 12
+ * @year : 2021
+ * @since : 0.1.0
+ **/
+public interface SuperBO {
+}
